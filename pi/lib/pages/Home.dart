5afd9b3483b2089c7d2c683f0adc733ef1ab1a4f
celestinhodev <gugamarquesgsm@gmail.com>
2023-08-07 21:27:52 -0300

    livros na pagina

--- pi/lib/pages/Home.dart
+++ pi/lib/pages/Home.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-
-// Constantes
 import '../constantes/cores.dart';
 
 class Home extends StatelessWidget {
@@ -14,28 +12,589 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.yellow,
       ),
       backgroundColor: paletteBlack,
-      body: Column(
+      body: SingleChildScrollView(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const Padding(
             padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
             child: Text(
-              'Lançamentos',
+              'Lançamento[s',
               style: TextStyle(
                 fontSize: 20,
                 color: paletteWhite,
               ),
             ),
           ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Image.asset(
-              'images/livros/livro.png',
-            ),
+          //const SizedBox(height: 20),
+          Row(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(30, 50, 5, 10),
+                      child: Image.asset(
+                        'images/livros/livro.png',
+                        height: 120,
+                      ),
+                    ),
+                    const Text(
+                      'Livro 1',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(5, 50, 10, 10),
+                      child: Image.asset(
+                        'images/livros/livro.png',
+                        height: 120,
+                      ),
+                    ),
+                    const Text(
+                      'Livro 2',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 50, 5, 10),
+                      child: Image.asset(
+                        'images/livros/livro.png',
+                        height: 120,
+                      ),
+                    ),
+                    const Text(
+                      'Livro 3',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(5, 50, 10, 10),
+                      child: Image.asset(
+                        'images/livros/livro.png',
+                        height: 120,
+                      ),
+                    ),
+                    const Text(
+                      'Livro 4',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
+
+          Row(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(30, 50, 5, 10),
+                      child: Image.asset(
+                        'images/livros/livro.png',
+                        height: 120,
+                      ),
+                    ),
+                    const Text(
+                      'Livro 1',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(5, 50, 10, 10),
+                      child: Image.asset(
+                        'images/livros/livro.png',
+                        height: 120,
+                      ),
+                    ),
+                    const Text(
+                      'Livro 2',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 50, 5, 10),
+                      child: Image.asset(
+                        'images/livros/livro.png',
+                        height: 120,
+                      ),
+                    ),
+                    const Text(
+                      'Livro 3',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(5, 50, 10, 10),
+                      child: Image.asset(
+                        'images/livros/livro.png',
+                        height: 120,
+                      ),
+                    ),
+                    const Text(
+                      'Livro 4',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+          Row(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(30, 50, 5, 10),
+                      child: Image.asset(
+                        'images/livros/livro.png',
+                        height: 120,
+                      ),
+                    ),
+                    const Text(
+                      'Livro 1',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(5, 50, 10, 10),
+                      child: Image.asset(
+                        'images/livros/livro.png',
+                        height: 120,
+                      ),
+                    ),
+                    const Text(
+                      'Livro 2',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 50, 5, 10),
+                      child: Image.asset(
+                        'images/livros/livro.png',
+                        height: 120,
+                      ),
+                    ),
+                    const Text(
+                      'Livro 3',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(5, 50, 10, 10),
+                      child: Image.asset(
+                        'images/livros/livro.png',
+                        height: 120,
+                      ),
+                    ),
+                    const Text(
+                      'Livro 4',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+          Row(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(30, 50, 5, 10),
+                      child: Image.asset(
+                        'images/livros/livro.png',
+                        height: 120,
+                      ),
+                    ),
+                    const Text(
+                      'Livro 1',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(5, 50, 10, 10),
+                      child: Image.asset(
+                        'images/livros/livro.png',
+                        height: 120,
+                      ),
+                    ),
+                    const Text(
+                      'Livro 2',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 50, 5, 10),
+                      child: Image.asset(
+                        'images/livros/livro.png',
+                        height: 120,
+                      ),
+                    ),
+                    const Text(
+                      'Livro 3',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(5, 50, 10, 10),
+                      child: Image.asset(
+                        'images/livros/livro.png',
+                        height: 120,
+                      ),
+                    ),
+                    const Text(
+                      'Livro 4',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+          Row(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(30, 50, 5, 10),
+                      child: Image.asset(
+                        'images/livros/livro.png',
+                        height: 120,
+                      ),
+                    ),
+                    const Text(
+                      'Livro 1',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(5, 50, 10, 10),
+                      child: Image.asset(
+                        'images/livros/livro.png',
+                        height: 120,
+                      ),
+                    ),
+                    const Text(
+                      'Livro 2',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 50, 5, 10),
+                      child: Image.asset(
+                        'images/livros/livro.png',
+                        height: 120,
+                      ),
+                    ),
+                    const Text(
+                      'Livro 3',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(5, 50, 10, 10),
+                      child: Image.asset(
+                        'images/livros/livro.png',
+                        height: 120,
+                      ),
+                    ),
+                    const Text(
+                      'Livro 4',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
         ],
       ),
+      )
     );
   }
 }
