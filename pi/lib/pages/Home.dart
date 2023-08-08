8c80e8f1082e484b603849fdb5f34e7960bb0676
celestinhodev <gugamarquesgsm@gmail.com>
2023-08-07 21:43:44 -0300

    reciclagem nos livros

--- pi/lib/pages/Home.dart
+++ pi/lib/pages/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pi/components/book_template.dart';
 
 // Constantes
 import '../components/drawer.dart';
@@ -25,7 +26,7 @@ class Home extends StatelessWidget {
           const Padding(
             padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
             child: Text(
-              'Lançamento[s',
+              'Lançamentos',
               style: TextStyle(
                 fontSize: 20,
                 color: paletteWhite,
@@ -35,114 +36,7 @@ class Home extends StatelessWidget {
           //const SizedBox(height: 20),
           Row(
             children: [
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const Home(),
-                    ),
-                  );
-                },
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(30, 50, 5, 10),
-                      child: Image.asset(
-                        'images/livros/livro.png',
-                        height: 120,
-                      ),
-                    ),
-                    const Text(
-                      'Livro 1',
-                      style: TextStyle(
-                        color: paletteWhite,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const Home(),
-                    ),
-                  );
-                },
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(5, 50, 10, 10),
-                      child: Image.asset(
-                        'images/livros/livro.png',
-                        height: 120,
-                      ),
-                    ),
-                    const Text(
-                      'Livro 2',
-                      style: TextStyle(
-                        color: paletteWhite,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const Home(),
-                    ),
-                  );
-                },
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(10, 50, 5, 10),
-                      child: Image.asset(
-                        'images/livros/livro.png',
-                        height: 120,
-                      ),
-                    ),
-                    const Text(
-                      'Livro 3',
-                      style: TextStyle(
-                        color: paletteWhite,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const Home(),
-                    ),
-                  );
-                },
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(5, 50, 10, 10),
-                      child: Image.asset(
-                        'images/livros/livro.png',
-                        height: 120,
-                      ),
-                    ),
-                    const Text(
-                      'Livro 4',
-                      style: TextStyle(
-                        color: paletteWhite,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              BookTemplate(caminhoImagem: 'images/livros/livro.png', nomeLivro: 'aaaaaa')
             ],
           ),
 
@@ -371,7 +265,6 @@ class Home extends StatelessWidget {
               ),
             ],
           ),
-
           Row(
             children: [
               GestureDetector(
@@ -484,7 +377,6 @@ class Home extends StatelessWidget {
               ),
             ],
           ),
-
           Row(
             children: [
               GestureDetector(
@@ -597,7 +489,6 @@ class Home extends StatelessWidget {
               ),
             ],
           ),
-
         ],
       ),
       )
