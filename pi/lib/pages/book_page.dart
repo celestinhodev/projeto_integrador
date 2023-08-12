7b0b7bc842afbd1e329880e6ae34bacb7fd5f26f
LinkGr <juangabrf@gmail.com>
2023-08-12 00:21:35 -0300

    Concertei a pagina de detalhes do livro que precisava do single chield scroll view

--- pi/lib/pages/book_page.dart
+++ pi/lib/pages/book_page.dart
@@ -38,191 +38,195 @@ class _BookDetailsPageState extends State<BookDetailsPage> {
     return Scaffold(
       appBar: BookTokAppBar,
       backgroundColor: paletteBlack,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            color: paletteBrown,
-            alignment: Alignment.center,
-            child: Stack(
-              children: [
-                CarouselSlider(
-                  items: [
-                    Hero(
-                      tag: nomeLivro,
-                      child: Image.asset('images/livros/livro.png'),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              color: paletteBrown,
+              alignment: Alignment.center,
+              child: Stack(
+                children: [
+                  CarouselSlider(
+                    items: [
+                      Hero(
+                        tag: nomeLivro,
+                        child: Image.asset('images/livros/livro.png'),
+                      ),
+                      Image.asset('images/livros/livro.png'),
+                      Image.asset('images/livros/livro.png'),
+                      Image.asset('images/livros/livro.png'),
+                    ],
+                    options: CarouselOptions(
+                      //Configurações do carrossel
+                      height: 400,
+                      enlargeCenterPage: true,
+                      scrollDirection: Axis.vertical,
+                      viewportFraction: 0.7,
                     ),
-                    Image.asset('images/livros/livro.png'),
-                    Image.asset('images/livros/livro.png'),
-                    Image.asset('images/livros/livro.png'),
-                  ],
-                  options: CarouselOptions(
-                    //Configurações do carrossel
-                    height: 400,
-                    enlargeCenterPage: true,
-                    scrollDirection: Axis.vertical,
-                    viewportFraction: 0.7,
+                  ),
+                  Positioned(
+                    right: 30,
+                    child: Container(
+                      height: 400,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Radio(
+                            value: 0,
+                            groupValue: _value,
+                            onChanged: (value) {
+                              _value = value;
+                            },
+                          ),
+                          Radio(
+                            value: 1,
+                            groupValue: _value,
+                            onChanged: (value) {
+                              _value = value;
+                            },
+                          ),
+                          Radio(
+                            value: 2,
+                            groupValue: _value,
+                            onChanged: (value) {
+                              _value = value;
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(30, 20, 30, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Nome Livro
+                  Text(
+                    nomeLivro,
+                    style: TextStyle(
+                      color: paletteWhite,
+                      fontSize: 20,
+                    ),
+                  ),
+        
+                  SizedBox(
+                    height: 20,
+                  ),
+        
+                  // Linha com Preço e Genero,
+                  Row(
+                    children: [
+                      // Preço
+                      Icon(
+                        Icons.shopping_cart,
+                        color: paletteWhite,
+                      ),
+                      Text(
+                        'R\$${precoLivro}',
+                        style: TextStyle(
+                          color: paletteWhite,
+                          fontSize: 20,
+                        ),
+                      ),
+        
+                      SizedBox(
+                        width: 50,
+                      ),
+        
+                      // Genero
+                      Icon(
+                        Icons.art_track,
+                        color: paletteWhite,
+                      ),
+                      Text(
+                        generoLivro,
+                        style: TextStyle(
+                          color: paletteWhite,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+        
+                  SizedBox(
+                    height: 10,
+                  ),
+        
+                  Divider(
+                    color: paletteWhite,
+                  ),
+        
+                  SizedBox(
+                    height: 10,
+                  ),
+        
+                  Text(
+                    'DESCRIÇÃO',
+                    style: TextStyle(
+                      color: paletteWhite,
+                      fontSize: 20,
+                    ),
+                  ),
+        
+                  SizedBox(
+                    height: 15,
+                  ),
+        
+                  Text(
+                    descricaoLivro,
+                    maxLines: 99999,
+                    softWrap: false,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      color: paletteWhite,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 60,),
+            
+            Row(
+              children: [
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      backgroundColor: paletteYellow,
+                    ),
+                    child: Text(
+                      'COMPRAR',
+                      style: TextStyle(
+                        color: paletteBlack,
+                      ),
+                    ),
                   ),
                 ),
-                Positioned(
-                  right: 30,
-                  child: Container(
-                    height: 400,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Radio(
-                          value: 0,
-                          groupValue: _value,
-                          onChanged: (value) {
-                            _value = value;
-                          },
-                        ),
-                        Radio(
-                          value: 1,
-                          groupValue: _value,
-                          onChanged: (value) {
-                            _value = value;
-                          },
-                        ),
-                        Radio(
-                          value: 2,
-                          groupValue: _value,
-                          onChanged: (value) {
-                            _value = value;
-                          },
-                        ),
-                      ],
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      backgroundColor: paletteBrown,
+                    ),
+                    child: Text(
+                      'ADICIONAR AO CARRINHO',
+                      style: TextStyle(
+                        color: paletteWhite,
+                      ),
                     ),
                   ),
                 ),
               ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(30, 20, 30, 0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Nome Livro
-                Text(
-                  nomeLivro,
-                  style: TextStyle(
-                    color: paletteWhite,
-                    fontSize: 20,
-                  ),
-                ),
-
-                SizedBox(
-                  height: 20,
-                ),
-
-                // Linha com Preço e Genero,
-                Row(
-                  children: [
-                    // Preço
-                    Icon(
-                      Icons.shopping_cart,
-                      color: paletteWhite,
-                    ),
-                    Text(
-                      'R\$${precoLivro}',
-                      style: TextStyle(
-                        color: paletteWhite,
-                        fontSize: 20,
-                      ),
-                    ),
-
-                    SizedBox(
-                      width: 50,
-                    ),
-
-                    // Genero
-                    Icon(
-                      Icons.art_track,
-                      color: paletteWhite,
-                    ),
-                    Text(
-                      generoLivro,
-                      style: TextStyle(
-                        color: paletteWhite,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-
-                SizedBox(
-                  height: 10,
-                ),
-
-                Divider(
-                  color: paletteWhite,
-                ),
-
-                SizedBox(
-                  height: 10,
-                ),
-
-                Text(
-                  'DESCRIÇÃO',
-                  style: TextStyle(
-                    color: paletteWhite,
-                    fontSize: 20,
-                  ),
-                ),
-
-                SizedBox(
-                  height: 15,
-                ),
-
-                Text(
-                  descricaoLivro,
-                  maxLines: 99999,
-                  softWrap: false,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    color: paletteWhite,
-                    fontSize: 16,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Spacer(),
-          Row(
-            children: [
-              Expanded(
-                child: TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    backgroundColor: paletteYellow,
-                  ),
-                  child: Text(
-                    'COMPRAR',
-                    style: TextStyle(
-                      color: paletteBlack,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    backgroundColor: paletteBrown,
-                  ),
-                  child: Text(
-                    'ADICIONAR AO CARRINHO',
-                    style: TextStyle(
-                      color: paletteWhite,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          )
-        ],
+            )
+          ],
+        ),
       ),
       bottomNavigationBar: BookTokNavigation(
         home: MyIconButtonNavigator(
