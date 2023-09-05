d2d5b8700de53e7883980a95d2257630156709a3
celestinhodev <gugamarquesgsm@gmail.com>
2023-09-04 21:36:23 -0300

    11

--- pi/lib/constantes/cores.dart
+++ pi/lib/constantes/cores.dart
@@ -7,3 +7,4 @@ const paletteBlack = Color(0xFF0D0D0D);
 const paletteGrey = Color.fromARGB(255, 126, 122, 113);
 const paletteDarkGrey = Color(0xFF272525);
 const paletteYellow2 = Color(0xFFF2D764);
+const paletteBlue = Color(0xFFF38B6FF);
--- pi/lib/pages/register.dart
+++ pi/lib/pages/register.dart
@@ -16,7 +16,8 @@ class _RegisterState extends State<Register> {
   TextEditingController nameEditingController = TextEditingController();
   TextEditingController emailEditingController = TextEditingController();
   TextEditingController passwordEditingController = TextEditingController();
-  TextEditingController confirmPasswordEditingController = TextEditingController();
+  TextEditingController confirmPasswordEditingController =
+      TextEditingController();
 
   Color getColor(Set<MaterialState> states) {
     const Set<MaterialState> interactiveStates = <MaterialState>{
@@ -32,94 +33,132 @@ class _RegisterState extends State<Register> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: paletteBlack,
-      body: Column(
-        children: [
-          Image.asset(
-            'Images/logo-login.png',
-            width: 500,
-            height: 200,
-          ),
-          registerTemplate(hintText: 'Nome Completo', isPassword: false, textEditingController: nameEditingController,),
-          const SizedBox(height: 25),
-          registerTemplate(hintText: 'Email', isPassword: false, textEditingController: emailEditingController,),
-          const SizedBox(height: 25),
-          registerTemplate(hintText: 'Senha', isPassword: true, textEditingController: passwordEditingController,),
-          const SizedBox(height: 25),
-          registerTemplate(hintText: 'Repita a Senha', isPassword: true, textEditingController: confirmPasswordEditingController,),
-          const SizedBox(height: 20),
-          
-          Padding(
-            padding: const EdgeInsets.fromLTRB(62, 0, 0, 0),
-            child: Row(
-              children: [
-                Transform.scale(
-                  scale: 2.0,
-                child: Checkbox(
-                  checkColor: paletteBlack,
-                  fillColor: MaterialStateProperty.resolveWith(getColor),
-                  value: isChecked,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      isChecked = value!;
-                    });
-                  },
-                ),
-              ),
-              const SizedBox(width: 80),
-              RichText(
-                text: const TextSpan(
-                  text:
-                      'Ao criar uma conta, você concorda com os\n',
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: paletteWhite,
+      body: Column(children: [
+        Image.asset(
+          'Images/logo-login.png',
+          width: 500,
+          height: 200,
+        ),
+        registerTemplate(
+          hintText: 'Nome Completo',
+          isPassword: false,
+          textEditingController: nameEditingController,
+        ),
+        const SizedBox(height: 25),
+        registerTemplate(
+          hintText: 'Email',
+          isPassword: false,
+          textEditingController: emailEditingController,
+        ),
+        const SizedBox(height: 25),
+        registerTemplate(
+          hintText: 'Senha',
+          isPassword: true,
+          textEditingController: passwordEditingController,
+        ),
+        const SizedBox(height: 25),
+        registerTemplate(
+          hintText: 'Repita a Senha',
+          isPassword: true,
+          textEditingController: confirmPasswordEditingController,
+        ),
+        const SizedBox(height: 40),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(80, 0, 0, 0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Transform.scale(
+                    scale: 2.0,
+                    child: Checkbox(
+                      checkColor: paletteBlack,
+                      fillColor: MaterialStateProperty.resolveWith(getColor),
+                      value: isChecked,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          isChecked = value!;
+                        });
+                      },
+                    ),
                   ),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: 'Termos de uso ',
+                  const SizedBox(width: 20),
+                  RichText(
+                    text: const TextSpan(
+                      text: 'Ao criar uma conta, você concorda com os\n',
                       style: TextStyle(
-                        color: Colors.blue,
-                      ),
-                    ),
-                    TextSpan(
-                      text: 'do aplicativo. Por favor,\n'
-                      'verifique a ',
-                      style: TextStyle(
+                        fontSize: 15,
                         color: paletteWhite,
                       ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'Termos de uso ',
+                          style: TextStyle(
+                            color: paletteBlue,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'do aplicativo. Por favor,\n'
+                              'verifique a ',
+                          style: TextStyle(
+                            color: paletteWhite,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'Notificação de privacidade\n',
+                          style: TextStyle(
+                            color:paletteBlue,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'Notificação de cookies ',
+                          style: TextStyle(
+                            color: paletteBlue,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'e a ',
+                          style: TextStyle(
+                            color: paletteWhite,
+                          ),
+                        ),
+                        TextSpan(
+                          text:
+                              'Notificação de\nanúncios baseados em interesse',
+                          style: TextStyle(
+                            color: paletteBlue,
+                          ),
+                        ),
+                      ],
                     ),
-                    TextSpan(
-                      text: 'Notificação de privacidade\n',
-                      style: TextStyle(
-                        color: Colors.blue,
-                      ),
-                    ),
-                    TextSpan(
-                      text: 'Notificação de cookies ',
-                      style: TextStyle(
-                        color: Colors.blue,
-                      ),
-                    ),
-                    TextSpan(
-                      text: 'e a ',
-                      style: TextStyle(
-                        color: paletteWhite,
-                      ),
-                    ),
-                    TextSpan(
-                      text:'Notificação de\nanúncios baseados em interesse',
-                      style: TextStyle(
-                        color: Colors.blue,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SubmittButton(
-                buttonText: 'aaaa', 
-                onPressed: print(''),
+                  ),
+                ],
               ),
             ],
+          ),
+        ),
+        const SizedBox(height: 50),
+        SubmittButton(
+          buttonText: 'Cadastrar',
+          onPressed: () {},
+        ),
+        Spacer(),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 22),
+          child: TextButton(
+            style: TextButton.styleFrom(
+              foregroundColor: paletteWhite,
+            ),
+            onPressed: () {
+              // Lógica aqui
+            },
+            child: const Text(
+              'Voltar ao login?',
+              style: TextStyle(
+                fontSize: 18,
+                color: paletteBlue,
+              ),
+            ),
           ),
         ),
       ]),
